62e9771a9a16efafba12c2b8ec4624450cd12d6a
jabhishekashok <jabhishekashok@gmail.com>
2023-08-19 14:49:09 +0530

    upadtes

--- Terraform/VMs/NOP_tf/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm-pub-ip" {
-  value = "http://${azurerm_linux_virtual_machine.testvm2.public_ip_address}"
-}
-output "mysql-db-name" {
-  value = azurerm_mysql_database.mysqldb.name
-}
-output "mysql-db-srvr-name" {
-  value = azurerm_mysql_server.mysqldbsrvr.name
-}
